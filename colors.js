2b7e5a526681931066d4a2b24ca1b6fb3cb5bc44
Daniel Richmond <dan@dgr.dev>
2025-11-03 11:39:50 -0600

    fix: change favorite icon to a heart

--- colors.js
+++ colors.js
@@ -90633,7 +90633,7 @@ function colorTemplate(color) {
               : "none"
           }" stroke="${generateAccessibleText(
     color
-  )}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" focusable="false"><polygon points="12 21 2 13 5 3 19 3 22 13 12 21"/></svg>
+  )}" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" aria-hidden="true" focusable="false"><path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z"/></svg>
         </button>
         <button aria-label="${
           hidden.includes(color.id) ? "Unhide" : "Hide"
