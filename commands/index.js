8be2a1192bb2d94d071ecf5266b0aad6a303e7e0
Daniel Richmond <daniel.g.richmond@gmail.com>
2026-02-24 00:16:21 -0600

    chore: remove unnecessary comments and alphabetize

--- commands/index.js
+++ commands/index.js
@@ -1,12 +1,7 @@
-/**
- * Command exports - Barrel file for Command Pattern implementation
- * Simplifies imports and provides a central point for command access
- */
-
-export { ToggleFavoriteCommand } from "./ToggleFavoriteCommand.js";
-export { ToggleHiddenCommand } from "./ToggleHiddenCommand.js";
 export { BulkFavoriteCommand } from "./BulkFavoriteCommand.js";
 export { BulkHideCommand } from "./BulkHideCommand.js";
-export { UnhideGroupCommand } from "./UnhideGroupCommand.js";
 export { ClearFavoritesCommand } from "./ClearFavoritesCommand.js";
 export { ClearHiddenCommand } from "./ClearHiddenCommand.js";
+export { ToggleFavoriteCommand } from "./ToggleFavoriteCommand.js";
+export { ToggleHiddenCommand } from "./ToggleHiddenCommand.js";
+export { UnhideGroupCommand } from "./UnhideGroupCommand.js";
